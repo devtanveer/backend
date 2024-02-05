4027b9bb88623d2c7f05197520dd85e57c8b9d05
devtanveer <158012528+devtanveer@users.noreply.github.com>
2024-02-05 19:10:28 +0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const corsOptions = {
-  origin: '*', // Change this to your actual frontend URL
+  origin: 'backend-lovat-mu.vercel.app', 
   credentials: true,
   methods: ["POST","GET","DELETE"],
   optionSuccessStatus: 200,
