20ec3128aef1c270f1eda08c8d85572330da76ba
devtanveer <158012528+devtanveer@users.noreply.github.com>
2024-02-23 00:54:43 +0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const corsOptions = {
-  origin: 'https://frontend-sooty-iota-88.vercel.app/', 
+  origin: 'https://frontend-sooty-iota-88.vercel.app', 
   credentials: true,
   methods: ["POST","GET","DELETE"],
   optionSuccessStatus: 200,
